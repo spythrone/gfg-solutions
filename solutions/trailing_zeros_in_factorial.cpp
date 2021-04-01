bba2ad66e6b949991fe89609e8d944911180a339
spythrone <pragyat021@gmail.com>
2021-04-01 21:33:37 +0530

    [ADD] Modified

--- solutions/trailing_zeros_in_factorial.cpp
+++ solutions/trailing_zeros_in_factorial.cpp
@@ -1,15 +1,33 @@
-#include<bits/stdc++.h>
+// { Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
 
-using namespace std;
 
-int main(){
-	int num;
-	cin>>num;
-	int cnt = 0;
-	vector<int> v(num+1, 0);
-	if(num<5) return 0;
-	for(int i=5; i<=num; i+=5){
-		v[i] = 1 + v[i/5];
-	}
-	return 0;
+class Solution
+{
+public:
+    int trailingZeroes(int N)
+    {
+        int cnt=0;
+        if(N<5) return 0;
+        for(int i=5; i<=N; i*=5){
+            cnt+=N/i;
+        }
+        return cnt;
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        int ans  = ob.trailingZeroes(N);
+        cout<<ans<<endl;
+    }
+    return 0;
 }
